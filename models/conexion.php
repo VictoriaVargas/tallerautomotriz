5cb1d43daf8139ba9e99e794208981aa7b20161e
JuliettaVargas <julieta.victoria.vargas@gmail.com>
2022-05-28 13:17:18 -0500

    Esta rama tiene lo suficiente para la entrega del día martes 31 de mayo

--- models/conexion.php
+++ models/conexion.php
@@ -3,8 +3,8 @@
 class Conexion{
     static public function conectar(){
         $link = new PDO("mysql:host=localhost;dbname=chromocars_db",
-            "root",
-            "",
+            "root", //Usuario de la base de datos
+            "",//Contraseña de acceso a la base de datos
             array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
             );
