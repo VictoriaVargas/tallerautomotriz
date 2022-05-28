bee56a94a328613b0d0c37b50aadcbf37c552744
JuliettaVargas <julieta.victoria.vargas@gmail.com>
2022-05-28 13:18:33 -0500

    Cambiando el titulo del proyecto

--- views/plantilla.php
+++ views/plantilla.php
@@ -7,7 +7,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="views/assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="views/assets/img/favicon.png">
   <title>
-    Proyecto de Titulación
+    Chromo Cars | Chile
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
